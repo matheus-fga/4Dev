f4b1f53fdef121e264c32cb29d04a8081902cbb2
Matheus Silva <matheussilva.fga@gmail.com>
2022-10-14 23:21:06 -0300

    feat: ensure that RemoteAuthentication returns an AccountModel on success

--- src/data/usecases/authentication/remoteAuthentication.ts
+++ src/data/usecases/authentication/remoteAuthentication.ts
@@ -3,22 +3,22 @@ import { HttpStatusCode } from '@/data/protocols/http/httpResponse'
 import { InvalidCredentialsError } from '@/domain/errors/invalidCredentialsError'
 import { UnexpectedError } from '@/domain/errors/UnexpectedError'
 import { AccountModel } from '@/domain/models/accountModel'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
+import { Authentication, AuthenticationParams } from '@/domain/usecases/authentication'
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor (
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
   ) {}
 
-  async auth (params: AuthenticationParams): Promise<void> {
+  async auth (params: AuthenticationParams): Promise<AccountModel> {
     const httpResponse = await this.httpPostClient.post({
       url: this.url,
       body: params
     })
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
-        break
+        return httpResponse.body
       case HttpStatusCode.unathorized:
         throw new InvalidCredentialsError()
       default:
