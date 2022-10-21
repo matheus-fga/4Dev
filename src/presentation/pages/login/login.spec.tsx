2f03b2d3c2dcd26d4d5b6dc349be470d03f0683f
Matheus Silva <matheussilva.fga@gmail.com>
2022-10-21 15:53:30 -0300

    test: ensure login page will not render spinner anb error on first render

---
+++ src/presentation/pages/login/login.spec.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import Login from './login'
+
+describe('Login component', () => {
+  test('should not render spinner and error on first render', () => {
+    const { getByTestId } = render(<Login />)
+    const loadingWrap = getByTestId('loading-wrap')
+    expect(loadingWrap.childElementCount).toBe(0)
+  })
+})
