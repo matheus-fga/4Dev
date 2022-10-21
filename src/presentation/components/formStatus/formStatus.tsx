791a41b3a2f4a3a7170ec4e425dad70250b8e726
Matheus Silva <matheussilva.fga@gmail.com>
2022-10-21 15:52:51 -0300

    feat: ensure login page will not render spinner anb error on first render

--- src/presentation/components/formStatus/formStatus.tsx
+++ src/presentation/components/formStatus/formStatus.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Styles from './formStatus-styles.scss'
 
 import { Spinner } from '@/presentation/components'
+
+import Context from '@/presentation/contexts/form/formContext'
 
 type FormStatusProps = {
   message: string
 }
 
 const FormStatus: React.FC<FormStatusProps> = ({ message }: FormStatusProps) => {
+  const { isLoading, errorMessage } = useContext(Context)
+
   return (
-    <div className={Styles.loadingWrap}>
-      <Spinner className={Styles.spinner} />
-      <span className={Styles.message}>{message}</span>
+    <div data-testid ="loading-wrap" className={Styles.loadingWrap}>
+      { isLoading && <Spinner className={Styles.spinner} /> }
+      { errorMessage && <span className={Styles.message}>{message}</span> }
     </div>
   )
 }
