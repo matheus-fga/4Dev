8f08488304080887569144da274eb16ecb242d84
Matheus Silva <matheussilva.fga@gmail.com>
2022-11-09 16:21:19 -0300

    feat: add Login composition

---
+++ src/main/factories/pages/login/loginFactory.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Login } from '@/presentation/pages'
+import { RemoteAuthentication } from '@/data/usecases/authentication/remoteAuthentication'
+import { AxiosHttpClient } from '@/infra/http/axiosHttpClient/axiosHttpClient'
+import { ValidationComposite } from '@/validation/validators'
+import { ValidationBuilder } from '@/validation/validators/builder/validationBuilder'
+
+export const makeLogin: React.FC = () => {
+  const url = 'http://fordevs.herokuapp.com/api/login'
+  const axiosHttpClient = new AxiosHttpClient()
+  const remoteAuthentication = new RemoteAuthentication(url, axiosHttpClient)
+  const validationComposite = ValidationComposite.build([
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().min(5).build()
+  ])
+
+  return (
+    <Login
+      authentication={remoteAuthentication}
+      validation={validationComposite}
+    />
+  )
+}
--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { Router } from '@/presentation/components'
+import { makeLogin } from './factories/pages/login/loginFactory'
 
 import '@/presentation/styles/global.scss'
 
 ReactDOM.render(
-  <Router />,
+  <Router
+    makeLogin={makeLogin}
+  />,
   document.getElementById('main')
 )
